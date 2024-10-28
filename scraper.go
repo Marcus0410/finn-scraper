0c83c169210b07e58f0a1cdf76fc70dc29fea6e6
marcusri <marcusri@uio.no>
2024-10-28 15:44:00 +0100

    added json functionality

--- scraper.go
+++ scraper.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gocolly/colly"
+	"log"
+	"os"
 	"sort"
 	"strings"
+
+	"github.com/gocolly/colly"
 )
 
 type Job struct {
-	title, publishedDate, url, company, deadline string
+	Title, PublishedDate, Url, Company, Deadline string
 }
 
 const visitURL = "https://www.finn.no/job/fulltime/search.html?location=1.20001.20061&occupation=0.23&q=nyutdannet"
@@ -34,22 +38,22 @@ func main() {
 	c.OnHTML("div.flex.flex-col",
 		func(h *colly.HTMLElement) {
 			job := Job{}
-			job.title = h.ChildText("h2")
-			job.company = h.DOM.Find("div.flex.flex-col.text-xs span").First().Text()
-			job.publishedDate = strings.Split(h.ChildText(".s-text-subtle"), "|")[0]
-			job.url = h.ChildAttr("h2 a", "href")
+			job.Title = h.ChildText("h2")
+			job.Company = h.DOM.Find("div.flex.flex-col.text-xs span").First().Text()
+			job.PublishedDate = strings.Split(h.ChildText(".s-text-subtle"), "|")[0]
+			job.Url = h.ChildAttr("h2 a", "href")
 
-			if job.title != "" && job.url != "" {
+			if job.Title != "" && job.Url != "" {
 				jobs = append(jobs, job)
-				deadlineCollector.Visit(job.url)
+				deadlineCollector.Visit(job.Url)
 			}
 		})
 
 	// scrape deadline
 	deadlineCollector.OnHTML("li.flex.flex-col", func(h *colly.HTMLElement) {
 		// if deadline has not been scraped
-		if jobs[len(jobs)-1].deadline == "" {
-			jobs[len(jobs)-1].deadline = h.DOM.Find("span.font-bold").First().Text()
+		if jobs[len(jobs)-1].Deadline == "" {
+			jobs[len(jobs)-1].Deadline = h.DOM.Find("span.font-bold").First().Text()
 		}
 	})
 
@@ -57,18 +61,48 @@ func main() {
 
 	// sort jobs based on publishedDate
 	sort.Slice(jobs, func(i, j int) bool {
-		return jobs[i].deadline < jobs[j].deadline
+		return jobs[i].Deadline < jobs[j].Deadline
 	})
 
 	fmt.Printf("Found %v jobs\n", len(jobs))
 	for _, job := range jobs {
 		fmt.Println("-----------------------")
-		fmt.Println("Title:", job.title)
-		fmt.Println("Company:", job.company)
-		fmt.Println("Published:", job.publishedDate)
-		fmt.Println("URL:", job.url)
-		fmt.Println("Deadline:", job.deadline)
+		fmt.Println("Title:", job.Title)
+		fmt.Println("Company:", job.Company)
+		fmt.Println("Published:", job.PublishedDate)
+		fmt.Println("URL:", job.Url)
+		fmt.Println("Deadline:", job.Deadline)
 		fmt.Println("-----------------------")
 		fmt.Println()
 	}
+
+	err := saveJobs(jobs)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// save jobs to json file
+func saveJobs(jobs []Job) error {
+	file, err := os.Create("jobs.json")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return json.NewEncoder(file).Encode(jobs)
+}
+
+// load previously stored jobs from json file
+func loadJobs() ([]Job, error) {
+	var jobs []Job
+	file, err := os.Open("jobs.json")
+	if err != nil {
+		return jobs, err
+	}
+	defer file.Close()
+
+	err = json.NewDecoder(file).Decode(&jobs)
+
+	return jobs, err
 }
